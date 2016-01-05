32fb9d103d609ce997a0046e69b8d9989b8f84a6
Alexey Kostyuk <unitoff@gmail.com>
2016-01-05 20:58:21 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='steams_logos',
+    version='0.0.1',
+    author='Alexey Kostyuk',
+    author_email='unitoff+steamslogos@gmail.com',
+    description=('Simple package that allows to extract sport teams logos '
+                 'from www.sportslogos.net'),
+    license='MIT',
+    keywords='sportslogos.net sport teams logos',
+    url='https://github.com/akostyuk/steams_logos',
+    packages=['steams_logos'],
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
